0691cc228fd568d4037f85593982635cb7416474
AnnaLara <annalarammv123@gmail.com>
2025-11-20 20:12:34 -0300

    feat: implement data persistence to save and load filesystem state from filesystem.dat

--- src/main/java/com/fs/system/FileSystemSimulator.java
+++ src/main/java/com/fs/system/FileSystemSimulator.java
@@ -2,19 +2,59 @@ package com.fs.system;
 
 import com.fs.model.Directory;
 import com.fs.model.File;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 public class FileSystemSimulator {
     private Directory root;
     private Directory currentDirectory;
-
     private Journal journal;
 
+    private static final String DATA_FILE_PATH = "storage/filesystem.dat";
+
     public FileSystemSimulator() {
-        this.root = new Directory("/");
-        this.currentDirectory = this.root;
         this.journal = new Journal();
+        if (!loadFileSystem()) {
+            this.root = new Directory("/");
+            this.currentDirectory = this.root;
+            System.out.println("System started from scratch (no backup found).");
+        } else {
+            System.out.println("System restored successfully!");
+        }
     }
+
+    private void saveFileSystem() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(DATA_FILE_PATH))) {
+            oos.writeObject(this.root);
+        } catch (IOException e) {
+            System.err.println("Error saving the file system: " + e.getMessage());
+        }
+    } 
+
+    private boolean loadFileSystem() {
+        java.io.File file = new java.io.File(DATA_FILE_PATH);
+        
+        if (!file.exists()) {
+            return false; 
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            this.root = (Directory) ois.readObject();
+            this.currentDirectory = this.root;
+            return true;
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error loading saved system: " + e.getMessage());
+            return false;
+        }
+    }
+
+
+
     public void createDirectory(String name) {
         if (currentDirectory.getSubDirectoryByName(name) != null) {
             System.out.println("Error: the directory '" + name + "' already exists.");
@@ -27,6 +67,7 @@ public class FileSystemSimulator {
 
         journal.log("CREATE_DIR", "Created directory '" + name + "' in " + currentDirectory.getName());
         
+        saveFileSystem();
     }
 
     public void createFile(String name, String content) {
@@ -41,6 +82,7 @@ public class FileSystemSimulator {
 
         journal.log("CREATE_FILE", "Created file '" + name + "' in " + currentDirectory.getName());
         
+        saveFileSystem();
     }
 
     public void listDirectory() {
